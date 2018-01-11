29fcc508e59870c9238c82a5dab19f6fd5c8ed84
Matthew Dworkin <mdworkin21@gmail.com>
2018-01-11 20:35:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -40,7 +40,7 @@ end
 
 
 def draw?(board)
- if won?(board) == WIN_COMBINATIONS.select{|win| win == WIN_COMBINATIONS[0]}
+ if won?(board) == WIN_COMBINATIONS.select{|win| win == WIN_COMBINATIONS[0][0] && win == WIN_COMBINATIONS[0][1] && win == WIN_COMBINATIONS[0][2]}
    return false
 elsif
   full?(board)
